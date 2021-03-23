572dd0eed1b6d35372c0a8a995ee3bad4de4ee42
JUNGKEE KIM <kee@JUNGKEEui-MacBookPro.local>
2021-03-23 11:45:11 +0900

    movie app done

---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./About.css";
+
+function about() {
+    return <span>About this page: I built it because I love movies</span>
+}
+
+export default about;
---
+++ src/routes/Detail.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const {location, history} = this.props;
+        if(location.state === undefined) {
+            history.push("/");
+        }
+    }
+    render() {
+        const { location } = this.props;
+        if(location.state) {
+            return <span>{location.state.title} </span>;
+        } else {
+            return null;
+        }
+    }
+}
+// function Detail({location}) {
+//     console.log(location);
+//     return <div>Hello! </div>
+// }
+
+export default Detail;
